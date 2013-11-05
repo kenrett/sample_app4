42d55aab87a1c5961a4631a0d698f220a3114a8c
kenrett <kenrettberg@gmail.com>
2013-11-04 17:22:09 -0800

    added link tests

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -32,4 +32,20 @@ describe "Static Pages" do
     it { should have_content('Contact') }
     it { should have_title(full_title('Contact')) }
   end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About Us'))
+    click_link "Help"
+    expect(page).to have_title(full_title('Help'))
+    click_link "Contact"
+    expect(page).to have_title(full_title('Contact'))
+    click_link "Home"
+    expect(page).to have_title(full_title(''))
+    click_link "Sign up now!"
+    expect(page).to have_title(full_title('Sign up'))
+    click_link "sample app"
+    expect(page).to have_title(full_title('Sign up'))
+  end
 end
